a0d3f9448d87c911690f2611721cec0f1eda9826
donggyu_cho <cho4036@gmail.com>
2021-09-28 04:52:52 +0000

    run.py update

--- build/run.py
+++ build/run.py
@@ -1,5 +1,12 @@
 import subprocess
 import os
+import getopt, sys
+
+PKG_NAME = 'github.com/cho4036/virtualrouter-controller'
+GO_BINARY_NAME = 'virtualrouter-controller'
+DOCKER_REGISTRY = '10.0.0.4:5000/'
+DOCKER_IMAGE_NAME = "virtualrouter-controller"
+DOCKER_IMAGE_TAG = "0.0.1"
 
 def subprocess_open(command):
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
@@ -42,36 +49,49 @@ def docker_push(name, tag, registry):
         return out, err
     return "", ""
 
-def main():
-    PKG_NAME = 'github.com/cho4036/virtualrouter-controller'
-    GO_BINARY_NAME = 'virtualrouter-controller'
-    DOCKER_REGISTRY = '10.0.0.4:5000/'
-    DOCKER_IMAGE_NAME = "virtualrouter-controller"
-    DOCKER_IMAGE_TAG = "0.0.1"
+def main(argv):
+    FILE_NAME = argv[0]
+    try :
+        if len(argv) < 2 :
+            print(FILE_NAME, "few arguments. Choose at least one option")
+            sys.exit(2)
 
-    out, err = go_build(package=PKG_NAME, output=GO_BINARY_NAME)
-    if err != "" or out != "":
-        print("Error: " + err + ", Out: " + out)
-        return
-    print("Go build done")
+        opts, etc_args = getopt.getopt(argv[1], "h", ["help", "gobuild"])
 
-    out, err = docker_build(name=DOCKER_IMAGE_NAME,tag=DOCKER_IMAGE_TAG, registry=DOCKER_REGISTRY)
-    if err != "":
-        print("Error: " + err)
-        return
-    print(out)
-    print("Docker build done")
-
-    out, err = docker_push(name=DOCKER_IMAGE_NAME,tag=DOCKER_IMAGE_TAG, registry=DOCKER_REGISTRY)
-    if err != "":
-        print("Error: " + err )
-        return
-    print(out)
-    print("Docker push done")
+    except getopt.GetoptError:
+        print(FILE_NAME, '--gobuild , --dockerbuild or --dockerpush')
+        sys.exit(2)
+    print(argv[1], "1")
+    for opt, arg in opts:
+        print(opts)
+        if opt in ("-h", "--help"):
+            print(FILE_NAME, '--gobuild , --dockerbuild or --dockerpush')
+            sys.exit()
+        elif opt in ("--gobuild"):
+            print("print", PKG_NAME, GO_BINARY_NAME)
+            out, err = go_build(package=PKG_NAME, output=GO_BINARY_NAME)
+            if err != "" or out != "":
+                print("Error: " + err + ", Out: " + out)
+                sys.exit(1)
+            print("Go build done")
+        elif opt in ("--dockerbuild"):
+            out, err = docker_build(name=DOCKER_IMAGE_NAME,tag=DOCKER_IMAGE_TAG, registry=DOCKER_REGISTRY)
+            if err != "":
+                print("Error: " + err)
+                sys.exit(1)
+            print(out)
+            print("Docker build done")
+        elif opt in ("dockerpush"):
+            out, err = docker_push(name=DOCKER_IMAGE_NAME,tag=DOCKER_IMAGE_TAG, registry=DOCKER_REGISTRY)
+            if err != "":
+                print("Error: " + err )
+                sys.exit(1)
+            print(out)
+            print("Docker push done")
 
     # os.chdir("../")
     # currentPath = os.getcwd()
     # print(currentPath)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
